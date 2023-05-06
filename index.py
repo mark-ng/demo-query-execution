61f9e3b4712f57e691b22ff1529214ecd25abf2a
markng <stngad@connect.ust.hk>
2023-05-07 01:45:44 +0800

    Implemented SeqScan and Sort

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
+from heapq import heappop, heappush
+
+def Select(it):
+    for row in it:
+        print(row)
+
 class InMemoryIterator:
     def __init__(self, rows):
         self.rows = rows
@@ -13,6 +19,36 @@ class InMemoryIterator:
         self.pos += 1
         return row
     
+class SeqScanIterator:
+    def __init__(self, it):
+        self.it = it
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        return next(self.it)
+    
+class SortIterator:
+    # TODO: Support multi-column sort
+    # TODO: support descending order
+    def __init__(self, col_idx, it):
+        self.col_idx = col_idx
+        self.it = it
+        self.heap = []
+    
+        # Sorting on init
+        for row in it:
+            heappush(self.heap, (row[self.col_idx], row))
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if len(self.heap) == 0:
+            raise StopIteration
+        return heappop(self.heap)[1]
+
 movies = [
     # movie_id, title, year
     (1, 'The Matrix', 1999),
@@ -20,7 +56,12 @@ movies = [
     (3, 'Harry Potter', 2003),
 ]
 
-movies_iterator = InMemoryIterator(movies)
+# SELECT * FROM movies;
+Select(SeqScanIterator(InMemoryIterator(movies)))
 
-for movie in movies:
-    print(movie)
+print()
+
+# SELECT * FROM movies ORDER BY title;
+Select(SortIterator(1, SeqScanIterator(InMemoryIterator(movies))))
+
+print()
