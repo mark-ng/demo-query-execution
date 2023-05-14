4d894049642a1d8ee805f28a9ecc95fc373b6e72
markng <stngad@connect.ust.hk>
2023-05-15 00:29:23 +0800

    Implemented a working nested loop join iterator

--- index.py
+++ index.py
@@ -89,6 +89,44 @@ class PredicateIterator:
         self._advance_to_match()
         return row
 
+class NestedLoopJoinIterator:
+    # TODO: According to the EXPLAIN result, the first item should be return right away \
+    # instead of doing two full table scan inside the init of the Iterator, one way to do \
+    # is use next() and keep track of the state
+
+    def __init__(self, it1, it2, col_idx1, col_idx2):
+        self.materialize_t1 = list(it1)
+        self.materialize_t2 = list(it2)
+        self.col_idx1 = col_idx1
+        self.col_idx2 = col_idx2
+        self.cur_row1 = 0
+        self.cur_row2 = 0
+        self.next_row = None
+        self._advance_to_match()
+
+    def _advance_to_match(self):
+        for idx1 in range(self.cur_row1, len(self.materialize_t1)):
+            for idx2 in range(self.cur_row2, len(self.materialize_t2)):
+                row1 = self.materialize_t1[idx1]
+                row2 = self.materialize_t2[idx2]
+                if row1[self.col_idx1] == row2[self.col_idx2]:
+                    self.next_row = row1 + row2
+                    self.cur_row2 = idx2 + 1
+                    return
+            self.cur_row2 = 0
+            self.cur_row1 = idx1 + 1
+        self.next_row = None
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.next_row == None:
+            raise StopIteration
+        row = self.next_row
+        self._advance_to_match()
+        return row
+
 movies = [
     # movie_id, title, year
     (1, 'The Matrix', 1999),
@@ -96,6 +134,17 @@ movies = [
     (3, 'Harry Potter', 2003),
     (4, 'The Matrix', 2000),
     (5, 'Pokemon', 2020)
+]
+
+ratings = [
+    # movie_id, rating
+    (1, 10),
+    (2, 9),
+    (3, 8),
+    (4, 5),
+    (5, 6),
+    (2, 8),
+    (10, 1)
 ]
 
 # SELECT * FROM movies;
@@ -117,3 +166,6 @@ print()
 # SELECT * FROM movies WHERE year >= 2003 ORDER BY year
 select(SortIterator(2, PredicateIterator(2, comparisonMatch(">=", 2003), SeqScanIterator(InMemoryIterator(movies)))))
 print()
+
+# SELECT * FROM movies JOIN ratings ON movies.movie_id = ratings.movie_id;
+select(NestedLoopJoinIterator(SeqScanIterator(InMemoryIterator(movies)), SeqScanIterator(InMemoryIterator(ratings)), 0, 0))
